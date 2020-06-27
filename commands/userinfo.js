37a603aaf9d0fe543e5a5a02515b1b0895c80fbd
MateHUN <matehunter123gmail.com>
2020-06-27 14:45:06 +0200

    sd

---
+++ commands/userinfo.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+const emo = require('../emoji.json');
+const szinek = require('../color.json');
+const moment = require('moment')
+exports.run = async (bot, message, args, prefix) =>
+{ 
+          const uembed = new Discord.RichEmbed()
+          .setColor(szinek.világos_kék)
+          .setTitle("User Info")
+          .setThumbnail(message.guild.iconURL)
+          .setAuthor(`${message.author.username} Info: `, message.author.displayAvatarURL)
+          .addField("**UserName:**", `${message.author.username}`, true)
+          .addField("**#ID:**", `${message.author.discriminator}`, true)
+          .addField("**DevID:**", `${message.author.id}`, true)
+          .addField("**SztátUsZ:**", `${message.author.presence.status}`, true)
+          .addField("**Created At:**", `${moment(message.author.createdAt).format('YYYY, MMMM, hh:mm:ss')}`, true)
+          .setFooter(`${bot.user.username} | Creator: !/Mééz\!MateHUN!/Mééz\!`, bot.user.displayAvatarURL);
+          message.channel.send({embed: uembed});
+};
