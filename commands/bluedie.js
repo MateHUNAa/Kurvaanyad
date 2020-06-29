bc070b816b4437b25cf310798f5315bf3d890c2f
MateHUN <matehunter123gmail.com>
2020-06-29 22:14:55 +0200

    Update bluedie.js

--- commands/bluedie.js
+++ commands/bluedie.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js');
 const emo = require('../emoji.json');
 const szinek = require('../color.json');
 const moment = require('moment')
-//
 exports.run = async (bot, message, args, prefix) =>
 { 
     if (message.author.id != '575342593630797825') return message.channel.send('Nincs Jogosultságod a használatára!')
