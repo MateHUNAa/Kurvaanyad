c1946b8e025a04c339b4d834fa43f90ffd1aeb03
MateHUN <matehunter123gmail.com>
2020-06-29 15:40:24 +0200

    Update lockdown.js

--- commands/lockdown.js
+++ commands/lockdown.js
@@ -6,18 +6,18 @@ const ms = require('ms')
 exports.run = async (bot, message, args, prefix) =>
 { 
 
-    if (message.author.id !== '575342593630797825') return message.channel.send(message.author.toString() + ' Nincs jogosultságod a használatára!')
+    if (message.author.id !== '575342593630797825', '480049452535775243') return message.channel.send(message.author.toString() + '\` Nincs jogosultságod a használatára!\`')
 
     if(!bot.lockit) bot.lockit = [];
     let time = args.join(' ');
     let validUnlocks = ['release', 'unlock'];
-    if (!time) return message.channel.send('Adj meg egy időt ameddig le legyen zárva a szoba!')
+    if (!time) return message.channel.send('\`Adj meg egy időt ameddig le legyen zárva a szoba!\`')
 
     if (validUnlocks.includes(time)) {
         message.channel.overwritePermissions(message.guild.id, {
             SEND_MESSAGES: null
         }).then(() => {
-            message.channel.send('lezárás \'kinyitása\'xD');
+            message.channel.send('\`lezárás feloldása!\`');
             clearTimeout(bot.lockit[message.channel.id]);
             delete bot.lockit[message.channel.id];
         }).catch(e => {
@@ -27,11 +27,11 @@ exports.run = async (bot, message, args, prefix) =>
         message.channel.overwritePermissions(message.guild.id, {
             SEND_MESSAGES: false
         }).then(() => {
-            message.channel.send(`A szoba le lett zárva: ${ms(ms(time), {long:true})} `).then(()=> {
+            message.channel.send(`\`A szoba le lett zárva: ${ms(ms(time), {long:true})} -ig \``).then(()=> {
                 bot.lockit[message.channel.id] = setTimeout(()=> {
                     message.channel.overwritePermissions(message.guild.id, {
                         SEND_MESSAGES: null
-                    }).then(message.channel.send('Szoba Már nincs le zárva!')).catch(console.error);
+                    }).then(message.channel.send('\`Szoba Már nincs le zárva!\`')).catch(console.error);
                     delete bot.lockit[message.channel.id];
                 }, ms(time));
             
