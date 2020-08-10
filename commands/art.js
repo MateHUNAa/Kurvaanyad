633b6ea043dacade0b9afd7a5473a202fbcfdf9c
MateHUN <matehunter123gmail.com>
2020-08-10 16:36:44 +0200

    Ascii Typing
    ?art

---
+++ commands/art.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+const emo = require('../emoji.json');
+const szinek = require('../color.json');
+const moment = require('moment')
+var figlet = require('figlet')
+exports.run = async (bot, message, args, prefix) =>
+{ 
+    if(!args.join(' ')) {
+        message.delete()
+        return message.channel.send('Adj meg egy szöveget').then(r => r.delete(3000))
+    }
+    figlet.text(args.join(' '), {
+        font: 'big',
+        horizontalLayout: 'default',
+        verticalLayout: 'default',
+        width: 1000,
+        whitespaceBreak: true
+    }, function(err, data) {
+        if(err) return console.dir(err)
+        message.channel.send(data, {
+            code: 'mb'
+        })
+})
+}
